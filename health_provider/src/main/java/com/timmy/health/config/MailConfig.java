96873fea1efcfcfccdeb91738c196a1fe8ac9e81
TimmyChung <examyou076@gmail.com>
2022-07-28 18:41:48 +0800

    add the MailConfiguration Class

---
+++ health_provider/src/main/java/com/timmy/health/config/MailConfig.java
@@ -0,0 +1,16 @@
+package com.timmy.health.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender(){
+        return new JavaMailSenderImpl();
+    }
+}
